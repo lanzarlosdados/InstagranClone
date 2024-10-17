680522e59eab9124f1fb1a4b564d43cb83aa3fa4
Fabian Zarate <56480007+lanzarlosdados@users.noreply.github.com>
2024-10-17 16:20:35 -0300

    create model and mock

---
+++ InstagranClone/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  InstagranClone
+//
+//  Created by fabian zarate on 17/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, mundo!")
+            }
+            .padding()
+        }
+        .
+    }
+}
+
+#Preview {
+    ContentView()
+}
