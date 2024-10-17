6d2686f215ba25cb69b8ef3d17cce77c7422ac6e
Fabian Zarate <56480007+lanzarlosdados@users.noreply.github.com>
2024-10-17 15:04:45 -0300

    Initial Commit

---
+++ InstagranClone/InstagranCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagranCloneApp.swift
+//  InstagranClone
+//
+//  Created by fabian zarate on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagranCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
